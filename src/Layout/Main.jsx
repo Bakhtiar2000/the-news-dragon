1afd5fdf20730ea36441ce7b3a797e576c846f1d
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-04-28 19:47:06 +0600

    Updated Header

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import Header from '../Pages/Shared/Header/Header';
-import { Outlet } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
+
 import Footer from '../Pages/Shared/Footer/Footer';
+import LeftNav from '../Pages/Shared/LeftNav/LeftNav';
+import RightNav from '../Pages/Shared/RightNav/RightNav';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
+            <Container>
+                <Row>
+                    <Col lg={3}>
+                        <LeftNav></LeftNav>
+                    </Col>
+                    <Col lg={6}>
+                        <h2>Main content coming</h2>
+                    </Col>
+                    <Col lg={3}>
+                        <RightNav></RightNav>
+                    </Col>
+                </Row>
+            </Container>
             <Footer></Footer>
-            {/* <Outlet></Outlet> */}
+
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
+import { Button, Container } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
 
 const Header = () => {
     return (
-        <div>
-            <h2 className='text-center'>Header</h2>
-        </div>
+        <Container>
+            <div className="text-center mt-5">
+                <img src={logo} alt="" />
+                <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
+                <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+            </div>
+            <div className='p-3 bg-secondary rounded d-flex gap-2 mb-3'>
+                <Button variant="danger">Latest</Button>{' '}
+                <Marquee speed={100} className='text-white'>
+                    Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
+                </Marquee>
+            </div>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className='mb-3 rounded'>
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link>
+                        </Nav>
+                        <Nav className='d-lg-flex align-items-lg-center gap-lg-3'>
+                            <Nav.Link href="#deets"><FontAwesomeIcon  icon={faUser} /></Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                            <Button variant="secondary">Login</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
     );
 };
 
