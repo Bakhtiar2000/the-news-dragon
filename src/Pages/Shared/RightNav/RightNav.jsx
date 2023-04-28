2326bb783e1e4d2942fbbcd4eb77973b9973808d
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-04-28 21:37:10 +0600

    Updated Right navbar

--- src/Pages/Shared/RightNav/RightNav.jsx
+++ src/Pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Right Nav</h2>
+            <h3>Login With</h3>
+            <Button className='mt-2 w-100' variant="outline-primary"><FaGoogle /> Login with google</Button>
+            <Button className='mt-2 w-100' variant="outline-secondary"><FaGithub />  Login with github</Button>
+            <div className='mt-4'>
+                <h3 className='mb-2'>Find us on</h3>
+                <ListGroup>
+                    <ListGroup.Item className='d-flex align-items-center gap-2'><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='d-flex align-items-center gap-2'><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='d-flex align-items-center gap-2'><FaInstagram /> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <img className='w-100 rounded' src={bg} alt="" />
+            </div>
         </div>
     );
 };
