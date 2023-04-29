e1385c1988d3d3de8cf210a2b022cce21f4a4896
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-04-29 11:05:03 +0600

    Updated all the links and created news details page

--- src/Layout/NewsLayout.jsx
+++ src/Layout/NewsLayout.jsx
@@ -3,23 +3,17 @@ import Header from '../Pages/Shared/Header/Header';
 import { Col, Container, Row } from 'react-bootstrap';
 
 import Footer from '../Pages/Shared/Footer/Footer';
-import LeftNav from '../Pages/Shared/LeftNav/LeftNav';
 import RightNav from '../Pages/Shared/RightNav/RightNav';
 import { Outlet } from 'react-router-dom';
 
-const Main = () => {
+const NewsLayout = () => {
     return (
         <div>
-            <Header></Header>
+             <Header></Header>
             <Container>
                 <Row>
-
-                    <Col lg={3}>
-                        <LeftNav></LeftNav>
-                    </Col>
-
-                    <Col lg={6}>
-                        <Outlet></Outlet> {/* রাউট অনুসারে কোন কিছু চেঞ্জ হইলে সেটা <outlet> এর মধ্যে থাকে */}
+                    <Col lg={9}>
+                        <Outlet></Outlet> 
                     </Col>
                     
                     <Col lg={3}>
@@ -28,9 +22,8 @@ const Main = () => {
                 </Row>
             </Container>
             <Footer></Footer>
-
         </div>
     );
 };
 
-export default Main;
+export default NewsLayout;
---
+++ src/Pages/Home/NewsCard/NewsCard.jsx
@@ -0,0 +1,51 @@
+import moment from 'moment';
+import React from 'react';
+import { Card, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar, FaStarHalfAlt } from 'react-icons/fa';
+import Rating from 'react-rating';
+
+const NewsCard = ({ news }) => {
+    const { _id, title, details, image_url, author, total_view, rating } = news
+    return (
+        <Card className="mb-4">
+            <Card.Header className='p-3 d-flex align-items-center gap-3'>
+                <Image style={{ width: '40px' }} src={author?.img} roundedCircle />
+                <div className='flex-grow-1'>
+                    <p className='mb-0 fw-semibold'>{author?.name}</p>
+                    <p className='mb-0'><small>{moment(author?.published_date).format('YYYY-MM-D')}</small></p>
+                </div>
+                <div className='d-flex gap-2'>
+                    <FaRegBookmark></FaRegBookmark>
+                    <FaShareAlt></FaShareAlt>
+                </div>
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Text>
+                    {
+                        details.length < 250 ?
+                            <>{details}</> :
+                            <>{details.slice(0, 250)}... <Link to={`/news/${_id}`}>Read More</Link></>
+                    }
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted">
+                <div className='flex-grow-1'>
+                    <Rating placeholderRating={rating.number}
+                    readonly
+                        emptySymbol={<FaRegStar  style={{color: '#FF8C47'}}></FaRegStar>}
+                        placeholderSymbol={<FaStar style={{color: '#FF8C47'}}></FaStar>}
+                        fullSymbol={<FaStar></FaStar>}></Rating>
+                    <span>{rating?.number}</span>
+                </div>
+                <div>
+                    <FaEye></FaEye> {total_view}
+                </div>
+            </Card.Footer>
+        </Card>
+    );
+};
+
+export default NewsCard;
